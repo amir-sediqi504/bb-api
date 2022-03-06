6959f42c83bdb9b0bf0cf85eb145ba7ab606f742
amir sediqi <amir.sediqi69@gmail.com>
2022-03-06 16:57:38 +0100

    set state

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import logo from '../img/logo.png'
 
 const Header = () => {
+  const [items, setItems] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
   return (
     <header className='center'>
       <img src={logo} alt='' />
