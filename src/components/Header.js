c06dc91a0fccee4efffea40fca2fb644a9bbfdbe
amir sediqi <amir.sediqi69@gmail.com>
2022-03-06 17:08:36 +0100

    applied useEffect & HTTP req

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,20 @@
 import React, {useState, useEffect} from 'react'
 import logo from '../img/logo.png'
-
+import axios from 'axios'
 const Header = () => {
   const [items, setItems] = useState([])
   const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    const fetchItems = async () =>{
+      const result = await axios(`https://www.breakingbadapi.com/api/quotes/1`)
+
+      console.log(result.data);
+      setItems(result.data) 
+      setIsLoading(false)
+    }
+    fetchItems()
+  }, [])
   return (
     <header className='center'>
       <img src={logo} alt='' />
